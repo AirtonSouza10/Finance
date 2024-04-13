14172a6d74a95424e44caeb313e31b3d20958ddd
AirtonSouza10 <airtonsouza.ti@gmail.com>
2024-04-13 11:17:07 -0300

    Acrescentei entidades pessoa, acesso, endereco

---
+++ src/main/java/com/finance/model/Acesso.java
@@ -0,0 +1,37 @@
+package com.finance.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+import org.springframework.security.core.GrantedAuthority;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Entity
+@Table(name = "acesso")
+@AllArgsConstructor
+@Data
+@SequenceGenerator(name = "seq_acesso", sequenceName = "seq_acesso", allocationSize = 1, initialValue = 1)
+public class Acesso implements GrantedAuthority {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_acesso")
+	private Long id;
+
+	@Column(nullable = false)
+	private String descricao; // ROLE_ADMIN
+
+	@Override
+	public String getAuthority() {
+		return null;
+	}
+
+}
