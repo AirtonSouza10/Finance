026a2f998c8de5542df2b3c49d4340271d82646b
AirtonSouza10 <airtonsouza.ti@gmail.com>
2024-08-03 15:21:56 -0300

    Delete Acesso

--- src/main/java/com/finance/controller/AcessoController.java
+++ src/main/java/com/finance/controller/AcessoController.java
@@ -4,12 +4,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.finance.model.Acesso;
+import com.finance.repository.AcessoRepository;
 import com.finance.service.AcessoService;
 
 @Controller
@@ -18,6 +20,8 @@ public class AcessoController {
 
 	@Autowired
 	private AcessoService acessoService;
+	@Autowired
+	private AcessoRepository acessoRepository;
 	
 	@ResponseBody
 	@PostMapping(value = "**/salvarAcesso")
@@ -26,4 +30,10 @@ public class AcessoController {
 		return new ResponseEntity<Acesso>(acessoSalvo, HttpStatus.OK);
 	}
 	
+	@ResponseBody
+	@DeleteMapping(value = "**/deletarAcesso")
+	public ResponseEntity<?> deletarAcesso(@RequestBody Acesso acesso) {
+		acessoRepository.delete(acesso);
+		return new ResponseEntity<>(HttpStatus.OK);
+	}
 }
--- src/test/java/com/finance/FinanceApplicationTests.java
+++ src/test/java/com/finance/FinanceApplicationTests.java
@@ -1,5 +1,7 @@
 package com.finance;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -17,14 +19,28 @@ class FinanceApplicationTests {
 	private AcessoController acessoController;
 
 	@Test
+	@Order(1)
 	void testCadastraAcesso() {
 		var acesso = new Acesso(null, "ROLE_ADMIN");
-		acessoServie.save(acesso);
+		acesso = acessoServie.save(acesso);
+		
+		Assertions.assertNotNull(acesso.getId());
 	}
 	
 	@Test
+	@Order(2)
 	void testCadastraAcessoController() {
 		var acesso = new Acesso(null, "ROLE_ADMIN");
-		acessoController.salvarAcesso(acesso);
+		acesso = acessoController.salvarAcesso(acesso).getBody();
+		
+		Assertions.assertEquals("ROLE_ADMIN", acesso.getDescricao());
 	}
+	
+	@Test
+	@Order(3)
+	void testDeletarAcessoController() {
+		var acesso = new Acesso(null, "ROLE_ADMIN");
+		acessoController.deletarAcesso(acesso);		
+	}
+
 }
